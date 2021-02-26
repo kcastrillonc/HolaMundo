f1bc9b6a06d298052352549c24f76d4123970e2d
Kevin Castrillón <kcastrillonc@unal.edu.co>
2021-02-26 15:34:04 +0000

    Espero que esto no salga mal

---
+++ hola.cpp
@@ -0,0 +1,10 @@
+#include <iostream> 
+//incluir la libreta de input output
+/*No tengo ni la más mínima idea de lo que estoy haciendo
+AAAAAAAAAAAAh*/
+int main(int arg, char **argv)
+//(<-->) para leer argumentos de la linea de comandos
+{
+  std::cout << "\nHola mundo:(\n\n";
+  return 0;
+}
